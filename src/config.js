346f767d093284f6836e38e7ced1be81669797eb
nukedart <ptdz@pm.me>
2020-02-08 22:56:20 +0000

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads22"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://d3fghui6d3.execute-api.us-east-1.amazonaws.com/prod/notes"
+    },
+    cognito: {
+      REGION: "YOUR_COGNITO_REGION",
+      USER_POOL_ID: "us-east-1_L2kBGUt3w",
+      APP_CLIENT_ID: "6ql1flu11gfop9cqgnrgkqlvql",
+      IDENTITY_POOL_ID: "us-east-1:19fc81e2-e0fa-42f2-be35-9aa9882610ed"
+    }
+  };
