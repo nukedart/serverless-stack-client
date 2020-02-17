70213c3d94ccb2f6e9f997562b86bba7afb64a2f
nukedart <ptdz@pm.me>
2020-02-17 14:17:07 +0000

    Building our React App

--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-1",
-      BUCKET: "notes-app-uploads22"
+      BUCKET: "notes-api-uploads22"
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://d3fghui6d3.execute-api.us-east-1.amazonaws.com/prod/notes"
+      URL: "https://d3fghui6d3.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
-      REGION: "YOUR_COGNITO_REGION",
+      REGION: "us-east-1",
       USER_POOL_ID: "us-east-1_L2kBGUt3w",
       APP_CLIENT_ID: "6ql1flu11gfop9cqgnrgkqlvql",
       IDENTITY_POOL_ID: "us-east-1:19fc81e2-e0fa-42f2-be35-9aa9882610ed"
